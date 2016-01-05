0e483a528078d6f4db0fc56c31b944977150f777
Spyers <cbccbc@CBCCBCdeMac-mini.local>
2016-01-05 14:11:09 +0800

    init

---
+++ PYTableViewIndex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PYTableViewIndex
+//
+//  Created by CBCCBC on 16/1/5.
+//  Copyright © 2016年 PY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PYTableViewIndex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PYTableViewIndex
+//
+//  Created by CBCCBC on 16/1/5.
+//  Copyright © 2016年 PY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
